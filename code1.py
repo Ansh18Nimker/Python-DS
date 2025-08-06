bff2f4f63c50452103bbd7031e2177fee92d9a5a
Ansh18Nimker <anshnimker2004@gmail.com>
2025-08-06 22:38:58 +0530

    done

---
+++ code1.py
@@ -0,0 +1,8 @@
+template = "dear {} you are a big boy now take {}$ bag"
+a = "john"
+b = "mary"
+c = 10000
+d = 20000
+
+
+print(f"{a} you are okay , now take this {c} bag")
---
+++ lambda func.py
@@ -0,0 +1,4 @@
+square = lambda x: x*x 
+print(square(3))
+sum = lambda z,y : z+y 
+print(sum(2,4))
